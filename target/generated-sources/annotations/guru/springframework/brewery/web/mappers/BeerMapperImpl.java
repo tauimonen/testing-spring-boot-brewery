a6cdc5c6aa864087083f94243a733e93191a06bc
vgtoui <toni.uimonen@tuni.fi>
2023-12-06 14:54:59 +0200

    Create BeerControllerIT

--- target/generated-sources/annotations/guru/springframework/brewery/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/brewery/web/mappers/BeerMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-06T11:41:44+0200",
+    date = "2023-12-06T14:38:45+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
